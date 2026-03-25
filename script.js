b7f8d8273bc5055027cb81a0e191d28542d1accc
EmirKilic <66028902+emoycuk@users.noreply.github.com>
2026-03-25 15:47:03 +0300

    remove uncredited spotify logic

--- script.js
+++ script.js
@@ -85,25 +85,17 @@ async function fetchAllData() {
         const liveStats = smartParseKworb(htmlText);
 
         // İŞTE ÇÖZÜM: Hardcode yerine otomatik ve dinamik dağıtım!
-        let totalUncredited = 0;
         Object.keys(liveStats).forEach(key => {
             // Total ve Orphan hariç tüm albümleri otomatik eşle
             if (key !== "TotalSpotify" && key !== "Orphan" && jtData.albums[key]) {
-                const uncredited = jtData.albums[key].streams.uncreditedSpotify || 0;
-                jtData.albums[key].streams.spotify = liveStats[key] + uncredited;
-                totalUncredited += uncredited;
+                jtData.albums[key].streams.spotify = liveStats[key];
             }
         });
 
         // Orphan özel durumu
         if (jtData.albums["Orphan"]) {
-            const uncreditedOrphan = jtData.albums["Orphan"].streams.uncreditedSpotify || 0;
-            jtData.albums["Orphan"].streams.spotify = liveStats.Orphan + uncreditedOrphan;
-            totalUncredited += uncreditedOrphan;
+            jtData.albums["Orphan"].streams.spotify = liveStats.Orphan;
         }
-
-        // Genel Spotify toplamına tüm uncredited streamleri ekle
-        liveStats.TotalSpotify += totalUncredited;
 
         // Tüm albümlerin YouTube verilerini paralel çek
         if (YOUTUBE_API_KEY) {
